5d9c464bf0b782cf88b3d975f632325f83cce117
Micah Murray <micah.murray@gmail.com>
2015-05-16 10:06:06 -0500

    Moves twilio JS into ajax callback

--- makecall.js
+++ makecall.js
@@ -1,67 +1,70 @@
 $(document).ready(function(){
 
     $.get('http://micahj.com/code/webphone/getToken.php',{secure:'true'},function(data){
-        Twilio.Device.setup( $.trim(data) ); 
-    });
+        var token = $.trim(data);
+        var connection=null;
+        
+        alert("got a twilio token: "+ token);
 
-	var connection=null;
-	
-	$("#call").click(function() {  
-		params = { "tocall" : $('#tocall').val()};
-		connection = Twilio.Device.connect(params);
-	});
-	$("#hangup").click(function() {  
-		Twilio.Device.disconnectAll();
-	});
-
-	Twilio.Device.ready(function (device) {
-		$('#status').text('Ready to start call');
-	});
-
-	Twilio.Device.incoming(function (conn) {
-		if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
-			connection=conn;
-		    conn.accept();
-		}
-	});
-
-	Twilio.Device.offline(function (device) {
-		$('#status').text('Offline');
-	});
-
-	Twilio.Device.error(function (error) {
-		$('#status').text(error);
-	});
-
-	Twilio.Device.connect(function (conn) {
-		$('#status').text("Successfully established call");
-		toggleCallStatus();
-	});
-
-	Twilio.Device.disconnect(function (conn) {
-		$('#status').text("Call ended");
-		toggleCallStatus();
-	});
-	
-	function toggleCallStatus(){
-		$('#call').toggle();
-		$('#hangup').toggle();
-		$('#dialpad').toggle();
-	}
-
-	$.each(['0','1','2','3','4','5','6','7','8','9','star','pound'], function(index, value) { 
-    	$('#button' + value).click(function(){ 
-			if(connection) {
-				if (value=='star')
-					connection.sendDigits('*')
-				else if (value=='pound')
-					connection.sendDigits('#')
-				else
-					connection.sendDigits(value)
-				return false;
-			} 
+        Twilio.Device.setup( token ); 
+        
+        $("#call").click(function() {  
+    		params = { "tocall" : $('#tocall').val()};
+    		connection = Twilio.Device.connect(params);
+    	});
+    	$("#hangup").click(function() {  
+    		Twilio.Device.disconnectAll();
+    	});
+    
+    	Twilio.Device.ready(function (device) {
+    		$('#status').text('Ready to start call');
+    	});
+    
+    	Twilio.Device.incoming(function (conn) {
+    		if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
+    			connection=conn;
+    		    conn.accept();
+    		}
+    	});
+    
+    	Twilio.Device.offline(function (device) {
+    		$('#status').text('Offline');
+    	});
+    
+    	Twilio.Device.error(function (error) {
+    		$('#status').text(error);
+    	});
+    
+    	Twilio.Device.connect(function (conn) {
+    		$('#status').text("Successfully established call");
+    		toggleCallStatus();
+    	});
+    
+    	Twilio.Device.disconnect(function (conn) {
+    		$('#status').text("Call ended");
+    		toggleCallStatus();
+    	});
+    	
+    	function toggleCallStatus(){
+    		$('#call').toggle();
+    		$('#hangup').toggle();
+    		$('#dialpad').toggle();
+    	}
+    
+    	$.each(['0','1','2','3','4','5','6','7','8','9','star','pound'], function(index, value) { 
+        	$('#button' + value).click(function(){ 
+    			if(connection) {
+    				if (value=='star')
+    					connection.sendDigits('*')
+    				else if (value=='pound')
+    					connection.sendDigits('#')
+    				else
+    					connection.sendDigits(value)
+    				return false;
+    			} 
 			});
-	});
-
+    	});
+        
+    });
 
 });
